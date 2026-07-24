8dec6eb580b3eff488b0c37d8618f1dd4df1e04a
sipfreeth <sipfreeth@gmail.com>
2026-07-24 16:25:30 +0700

    Delete api/admin/logout.js

--- api/admin/logout.js
+++
@@ -1,8 +0,0 @@
-// api/admin/logout.js
-import { clearSessionCookie } from '../../lib/adminAuth.js';
-
-export default async function handler(req, res) {
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  res.writeHead(302, { Location: '/api/admin/login' });
-  res.end();
-}
